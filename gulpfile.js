26fd39bb1408e17ce785bef92337f5500084f263
MerrryL <48694827+MerrryL@users.noreply.github.com>
2020-01-05 03:08:22 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require ('gulp');
+const sass = require ('gulp-sass');
+const browserSync = require ('browser-sync').create();
+
+//compile scss into css
+function style(){
+    //1. where is my scss file
+    return gulp.src('./scss/**/*.scss')
+
+    //2. pass that file through sass compiler
+    .pipe(sass())
+
+    //3. where do i save the compiled css?
+    .pipe(gulp.dest('./css'))
+
+    //4. synch it to browsers
+    .pipe(browserSync.stream());
+}
+
+function watch(){
+    browserSync.init({
+        server:{
+            baseDir:'./'
+        }
+    });
+    
+    gulp.watch('./scss/**/*.scss', style);
+    gulp.watch('./css/**/*.css').on('change', browserSync.reload);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
